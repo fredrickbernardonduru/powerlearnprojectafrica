116b2ce7da0da818296489a426d7c00596bb0f5a
Fredrick Bernard Onduru <fredrickbernardonduru@gmail.com>
2024-11-21 15:24:35 +0300

    First commit

---
+++ week 1 assignment.py
@@ -0,0 +1,31 @@
+def calculator():
+    print("Simple Calculator")
+    
+    try:
+        num1 = float(input("Enter the first number: "))
+        num2 = float(input("Enter the second number: "))
+        operation = input("Enter the operation (+, -, *, /): ").strip()
+        
+
+        if operation == '+':
+            result = num1 + num2
+            print(f"{num1} + {num2} = {result}")
+        elif operation == '-':
+            result = num1 - num2
+            print(f"{num1} - {num2} = {result}")
+        elif operation == '*':
+            result = num1 * num2
+            print(f"{num1} * {num2} = {result}")
+        elif operation == '/':
+            if num2 != 0:
+                result = num1 / num2
+                print(f"{num1} / {num2} = {result}")
+            else:
+                print("Error: Division by zero is not allowed.")
+        else:
+            print("Error: Invalid operation.")
+    except ValueError:
+        print("Error: Please enter valid numbers.")
+
+
+calculator()
